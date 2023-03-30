e3746ef4da5b80171120837c36f2b13d2f814921
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-03-30 16:07:37 +0200

    Array STOPS definiert und angezeigt

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let stop_lng = 175.583333;
 let zoom = 13;
 let title = 'Tongariro-Nationalpark';
 
+const STOPS = ["Wellington", "Tongariro-Nationalpark", "Rotorua"];
+console.log(STOPS);
+console.log(STOPS[0]);
+console.log(STOPS[1]);
+console.log(STOPS[2]);
+console.log(STOPS.length);
+
 let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
