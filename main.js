acebf0fcbcd931df0582a7dd264afb801d558864
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-03-30 16:43:08 +0200

    for of Schleife für die Stops definiert

--- main.js
+++ main.js
@@ -91,7 +91,6 @@ const STOPS = [
         wikipedia: "https://de.wikipedia.org/wiki/Auckland"
     }
 ];
-console.log(STOPS);
 
 let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
@@ -102,3 +101,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([stop_lat, stop_lng]).addTo(map)
     .bindPopup(title)
     .openPopup();
+
+for (let stop of STOPS) {
+    //console.log(stop);
+    console.log(stop.title);
+    console.log(stop.user);
+    console.log(stop.lat);
+    console.log(stop.lng);
+    console.log(stop.wikipedia);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
